3c947584eebe6b727e22d9aca9fc6db15cec0f90
MO-ZAREI84 <zareisanim@gmail.com>
2024-08-23 12:30:11 +0330

    third test

--- book/models.py
+++ book/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
-class family(models.Model):
-    name=models.CharField(max_length=20)
+class Author (models.Model):
+    name=models.CharField(max_length=10)
+
+class Book(models.Model):
+    title=models.CharField(max_length=20)
+    authors=models.ManyToManyField(Author)
+    
+class Profile(models.Model):
+    email=models.EmailField()
+    phone_number=models.IntegerField()
+
+class Personal(models.Model):
+    profile=models.OneToOneField(Profile,on_delete=models.CASCADE)
