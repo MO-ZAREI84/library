135d1ccdbd8a62745fdac94a35765f0d66039527
MO-ZAREI84 <zareisanim@gmail.com>
2024-08-24 15:06:21 +0330

    fourth commit

--- book/models.py
+++ book/models.py
@@ -6,10 +6,23 @@ class Author (models.Model):
 class Book(models.Model):
     title=models.CharField(max_length=20)
     authors=models.ManyToManyField(Author)
+# برای وارد کردن دیتا many to many
+# >>> Author5=Author.objects.create(name='mostafa')
+# >>> book.authors.add(Author5)
+# >>> books=Book.objects.all()
+# >>> for book in books:
+# ...     author_names=book.authors.values_list('name',flat=True)
+# ...     print(f"book:{book.title} authors={author_names}")
+# ...
+# book:saa authors=<QuerySet []>
+# book:My First Book authors=<QuerySet ['Author1', 'Author2']>
+# book:pirmard va darya authors=<QuerySet ['tolsoi']>
+# book:1984 authors=<QuerySet ['jorj']>
+# book:sahab authors=<QuerySet ['mostafa']>
     
 class Profile(models.Model):
-    email=models.EmailField()
+    email=models.EmailField(default='default@example.com')
     phone_number=models.IntegerField()
 
-class Personal(models.Model):
+class Person(models.Model):
     profile=models.OneToOneField(Profile,on_delete=models.CASCADE)
